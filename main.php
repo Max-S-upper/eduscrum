bc2916cee3b82e739954fafe37e23df87bca9871
vagrant <vagrant@homestead>
2019-12-21 10:38:27 +0000

    style Home page

--- main.php
+++ main.php
@@ -19,7 +19,7 @@
 		<ul>
 			<li><a href="/">Home</a></li>
 			<li><a href="/lists">Lists</a></li>
-			<li><a href="/groups">Groups</a></li>
+			 <li><a href="/groups">Groups</a></li>
 		</ul>
 		<div class="profile">
 			<p><a href="/profile">Profile</a></p> <img src="images/ico-profile.png" alt="profile"> <a href="/signOut.php">Sign out</a>
